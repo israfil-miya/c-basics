e94549d2d1288735e48c2386614d9087413657b8
Israfil Miya <israfilmiya120@gmail.com>
2026-05-08 02:56:30 +0600

    Change WeekDay enum to start from zero

--- programs/include/week_day.h
+++ programs/include/week_day.h
@@ -1,17 +1,17 @@
 // Enum to represent the days of the week
-typedef enum WeekDay // can be unnamed but it's better to name it for clarity
+typedef enum WeekDay
 {
-    Sunday = 1,
-    Monday = 2,
-    Tuesday = 3,
-    Wednesday = 4,
-    Thursday = 5,
-    Friday = 6,
-    Saturday = 7
+    Sunday = 0,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
 } weekday;
 
 // Struct to represent a day of the week
-typedef struct Day // can be unnamed but it's better to name it for clarity
+typedef struct Day
 {
     weekday day_index;
 } day;
