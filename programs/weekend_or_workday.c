deaf24f6cb5de723df208a5bb52f95fca89e03c5
Israfil Miya <israfilmiya120@gmail.com>
2026-05-08 02:57:42 +0600

    Refactor weekend or workday program
    Refactor code to improve readability and error handling.

--- programs/weekend_or_workday.c
+++ programs/weekend_or_workday.c
@@ -3,25 +3,27 @@
 #include <stdlib.h>
 #include "include/week_day.h"
 
-int main()
+int main(void)
 {
-    day weekend1 = {Sunday};
-    day weekend2 = {Saturday};
+    day weekend1 = {.day_index = Sunday};
+    day weekend2 = {.day_index = Saturday};
 
     time_t t = time(NULL);
-    struct tm *tm_info = NULL;
 
-    tm_info = (struct tm *)malloc(sizeof(struct tm));
+    struct tm *tm_info = localtime(&t);
+
     if (tm_info == NULL)
     {
-        fprintf(stderr, "Memory allocation failed\n");
+        fprintf(stderr, "Failed to get local time.\n");
         return 1;
     }
 
-    tm_info = localtime(&t); // localtime() returns a pointer to a static tm structure, so we need to copy it to our allocated memory
-
-    // for pointers to structs, use ( -> ) to access members
-    int today = tm_info->tm_wday + 1; // tm_wday is 0-indexed
+    // tm_wday:
+    // 0 = Sunday
+    // 1 = Monday
+    // ...
+    // 6 = Saturday
+    weekday today = (weekday)tm_info->tm_wday;
 
     if (today == weekend1.day_index || today == weekend2.day_index)
     {
